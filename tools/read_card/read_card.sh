986d8a2f1cb38238ba0f7b5edab2f647ff13d59d
Cameron Goddard <camerongoddard@me.com>
2024-10-31 22:55:06 -0400

    add read card shell script

---
+++ tools/read_card/read_card.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+#
+# Reads the contents of data.csv from the Adalogger and writes it out to a similar file in this directory.
+#
+
+FQBN="adafruit:samd:adafruit_feather_m0"
+PORT=$(arduino-cli board list | grep "$FQBN" | awk '{print $1}')
+
+if [ -z "$PORT" ]; then
+    echo "Error: No Adalogger detected"
+    exit 1
+else
+    echo "Adalogger detected on port $PORT"
+fi
+
+arduino-cli compile --fqbn "$FQBN" read_card.ino
+arduino-cli upload -p "$PORT" --fqbn "$FQBN" read_card.ino
+
+python3 save_card.py "$PORT"
--- tools/read_card/save_card.py
+++ tools/read_card/save_card.py
@@ -1,11 +1,14 @@
+"""
+Receives data over serial and writes it to a copy of data.csv
+"""
 import serial
 import time
+import sys
 
-serial_port = "/dev/tty.usbmodem101"
 baud_rate = 9600
 file_name = "data.csv"
 
-def save_card() -> None:
+def save_card(serial_port: str) -> None:
     ser = serial.Serial(serial_port, baud_rate)
 
     with open(file_name, "wb") as output_file:
@@ -25,4 +28,9 @@ def save_card() -> None:
     print("\nFile saved successfully")
 
 if __name__ == "__main__":
-    save_card()
+    if len(sys.argv) < 2:
+        print("Usage: python save_card.py <port>")
+        sys.exit(1)
+
+    serial_port = sys.argv[1]
+    save_card(serial_port)
