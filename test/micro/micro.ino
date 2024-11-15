7003eb86c61cc7b03c6d62e3819449b6f472e5be
Cameron Goddard <camerongoddard@me.com>
2024-11-15 15:57:57 -0500

    update unit tests

---
+++ test/micro/micro.ino
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <SD.h>
+
+#define SD_PIN 4
+#define BUILTIN_LED_PIN 13
+
+int counter = 0;
+
+void setup() {
+    Serial.begin(9600);
+    
+    pinMode(BUILTIN_LED_PIN, OUTPUT);
+    digitalWrite(BUILTIN_LED_PIN, HIGH);
+
+    while (!Serial);
+
+    if (!SD.begin(SD_PIN)) {
+        while (true) {
+            Serial.println("Error: SD interface failed to initialize");
+            delay(1000);
+        }
+    }
+}
+
+void loop() {
+    File writeFile = SD.open("test.txt", FILE_WRITE);
+    writeFile.println(counter);
+    writeFile.flush();
+    writeFile.close();
+
+    File readFile = SD.open("test.txt", FILE_READ);
+    if (readFile) {
+        while (readFile.available()) {
+            char data = readFile.read();
+            Serial.print(data);
+        }
+        Serial.println();
+    } else {
+        Serial.println("Error: Failed to read file");
+    }
+
+    delay(3000);
+}
