4e6feac07a08b45c091c8738ef3eba61a7e0af79
Cameron Goddard <camerongoddard@me.com>
2025-04-21 17:01:18 -0400

    set guide laser pin low

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ void setup() {
 
     pinMode(GATE_PIN, OUTPUT);
     pinMode(GUIDE_LASER_PIN, OUTPUT);
+    digitalWrite(GUIDE_LASER_PIN, LOW);
 
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     attachInterrupt(BUTTON_PIN, set_automated, FALLING);
